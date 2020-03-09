47fe0cf5d2001a75833762f1bc00fa72a5b82228
ppaczula <ppaczula@future-processing.com>
2020-03-09 15:03:13 +0100

    Add docker https support, at least tried

--- DemoAPI.Server/Program.cs
+++ DemoAPI.Server/Program.cs
@@ -2,10 +2,14 @@ using DemoAPI.Common;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Net;
+
 namespace GRPC.API
 {
     public class Program
     {
+        public static string CERT_PASSWORD => Environment.GetEnvironmentVariable("certPassword") ?? "1234";
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -16,8 +20,17 @@ namespace GRPC.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseKestrel(opts => opts.ConfigureEndpointDefaults(o => o.Protocols = HttpProtocols.Http2));
-                    var host = Configuration.UrlForServer ?? "https://+:5000";
+                    webBuilder.UseKestrel(opts =>
+                    {
+                        opts.Listen(IPAddress.Any, Configuration.AppPort,
+                        o =>
+                            {
+                                o.UseHttps("cert.pfx", CERT_PASSWORD);
+                            });
+                        opts.ConfigureEndpointDefaults(o => o.Protocols = HttpProtocols.Http2);
+
+                    });
+                    var host = "https://+:" + Configuration.AppPort;
                     webBuilder.UseUrls(host);
                 });
     }
